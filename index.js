ad0f95c2c0ffb7780e953cbf4d79f370480e54cd
Gustavogoncal <67110868+Gustavogoncal@users.noreply.github.com>
2022-05-02 14:56:10 -0300

    Versão 1.1

--- index.js
+++ index.js
@@ -7,12 +7,34 @@ let resultado = document.getElementById("res")
 function calcular(){
     console.log("Calculando...")
 
-    let idaVolta = inputIdaVolta.value
-    let litrosCombus = inputLitrosCombus.value
-    let kmL = inputKmL.value
+    let idaVolta = parseFloat(inputIdaVolta.value)
+    let litrosCombus = parseFloat(inputLitrosCombus.value)
+    let kmL = parseFloat(inputKmL.value)
 
     let calculo = (idaVolta / kmL) * litrosCombus
 
     resultado.innerHTML = (calculo).toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })
     
+}
+
+function mascara(i, t){
+
+    var v = i.value
+
+    if(isNaN(v[v.length-1])){
+        i.value = v.substring(0,v.length-1)
+        return
+    }
+
+    if(t == "km"){
+        i.setAttribute("maxlength", 4)
+    }
+
+    if(t == "valor"){
+        i.setAttribute("maxlength", 2)
+    }
+
+    if(t == "kmLitros"){
+        i.setAttribute("maxlength", 2)
+    }
 }
